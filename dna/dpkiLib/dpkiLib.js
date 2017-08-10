7ed1c982111295e6c73324fbbde0e637422b5b46
Joel U <joelulahanna@gmail.com>
2017-08-10 11:05:43 -0700

    register_apps and get_user_details function Done

--- dna/dpkiLib/dpkiLib.js
+++ dna/dpkiLib/dpkiLib.js
@@ -26,26 +26,67 @@ function doGetTagLinkLoad(base, tag) {
 //  by passing a public_key of the users they want to verify (i.e if he exist in the DHT of the DPKI)
 
 
-function verifyUser(agent_id){
-if(!getAgent(agent_id)){
-  return false
+
+function bridgeGenesis()
+{
+  return true;
 }
-else{
+
+function receive(from, arg)
+{
+  if(arg.type == "verifyUser")
+  {
+    var val = verifyUser(arg.app_agent_id)
+  }
+  else if(arg.type == "registerUserApp")
+  {
+    var val=registerUserApp(arg.app_agent_id)
+  }
+  return val;
+}
+
+function registerUserApp(app_agent_id){
+  me=App.Agent.Hash
+  app_agent_id_hash=commit("app_agent_id",app_agent_id);
+  commit("app_agent_id_link", {Links:[{Base:me,Link:app_agent_id_hash,Tag:"app_agent_id"}]});
+  debug("app_agent_id_link: "+JSON.stringify(getLink(me,"app_agent_id",{Load:true})));
+  a=getLink(me,"app_agent_id",{Load:true})
   return true
 }
+
+
+function verifyUser(app_agent_id){
+  var sources = get(app_agent_id,{GetMask:HC.GetMask.Sources});
+  debug("Sources of the app_agent: "+sources)
+  if (isErr(sources)) {sources = [];}
+  if (sources != undefined) {
+      var n = sources.length -1;
+      return (n >= 0) ? sources[n] : false;
+  }
+  return false;
 }
+
+function getUserDetails(app_agent_id){
+source=getAgent(app_agent_id);
+if(source==""){
+  return false
+}else{
+  a=getLink(source,"users",{Load:true})
+  return a.Links[0].E
+}
+
+}
+
 //This is just going to check if the userAddress that was given actually exits
-function getAgent(handleHash) {
-    var directory = App.DNA.Hash;
-  //  var handleHash = makeHash(handle);
-    var sources = get(handleHash,{GetMask:HC.GetMask.Sources});
-debug("Sources: "+sources)
+function getAgent(app_agent_id) {
+    var sources = get(app_agent_id,{GetMask:HC.GetMask.Sources});
+    debug("Sources of the app_agent: "+sources)
     if (isErr(sources)) {sources = [];}
     if (sources != undefined) {
         var n = sources.length -1;
-        return (n >= 0) ? sources[n] : false;
+        return (n >= 0) ? sources[n] : "";
     }
-    return false;
+    return "";
 }
 function isErr(result) {
     return ((typeof result === 'object') && result.name == "HolochainError");
