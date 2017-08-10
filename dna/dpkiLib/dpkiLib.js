59697737dec6e9eead4416ee75198180411d32be
Joel U <joelulahanna@gmail.com>
2017-08-10 13:36:04 -0700

    Updated the registration of users and retrival of user details

--- dna/dpkiLib/dpkiLib.js
+++ dna/dpkiLib/dpkiLib.js
@@ -46,11 +46,13 @@ function receive(from, arg)
 }
 
 function registerUserApp(app_agent_id){
-  me=App.Agent.Hash
-  app_agent_id_hash=commit("app_agent_id",app_agent_id);
-  commit("app_agent_id_link", {Links:[{Base:me,Link:app_agent_id_hash,Tag:"app_agent_id"}]});
-  debug("app_agent_id_link: "+JSON.stringify(getLink(me,"app_agent_id",{Load:true})));
-  a=getLink(me,"app_agent_id",{Load:true})
+  keyRegistration=getLink(App.Agent.Hash,"keyRegistration",{Load:true});
+  debug(keyRegistration)
+  keyRegistrationHash=makeHash(keyRegistration.Links[0].E)
+  z=commit("app_agent_id_link", {Links:[{Base:app_agent_id,Link:keyRegistrationHash,Tag:"app_agent_id"}]});
+  debug(z)
+  debug("app_agent_id_link: "+JSON.stringify(getLink(app_agent_id,"app_agent_id",{Load:true})));
+  a=getLink(app_agent_id,"app_agent_id",{Load:true})
   return true
 }
 
@@ -66,17 +68,22 @@ function verifyUser(app_agent_id){
   return false;
 }
 
+
 function getUserDetails(app_agent_id){
-source=getAgent(app_agent_id);
-if(source==""){
-  return false
-}else{
-  a=getLink(source,"users",{Load:true})
-  return a.Links[0].E
+  key=getLink(app_agent_id,"app_agent_id",{Load:true})
+  source=JSON.parse(key.Links[0].E)
+  a=getLink(source.perm_dpki_id,"users",{Load:true})
+  userDetails=JSON.parse(a.Links[0].E)
+  arg={
+    key : key.Links[0].E,
+    shared_ID : userDetails.shared_ID,
+    address : userDetails.address,
+    email : userDetails.email
+  }
+  debug(arg);
+  return JSON.stringify(arg)
 }
-
-}
-
+/*
 //This is just going to check if the userAddress that was given actually exits
 function getAgent(app_agent_id) {
     var sources = get(app_agent_id,{GetMask:HC.GetMask.Sources});
@@ -91,3 +98,57 @@ function getAgent(app_agent_id) {
 function isErr(result) {
     return ((typeof result === 'object') && result.name == "HolochainError");
   }
+*/
+
+
+  // ===============================================================================
+  //   VALIDATION functions
+  // ===============================================================================
+
+  function validateCommit(entry_type,entry,header,pkg,sources) {
+      debug("validate commit: "+entry_type);
+      return validate(entry_type,entry,header,sources);
+  }
+
+  function validatePut(entry_type,entry,header,pkg,sources) {
+      debug("validate put: "+entry_type);
+      return validate(entry_type,entry,header,sources);
+  }
+
+  function validate(entry_type,entry,header,sources) {
+      return true;
+  }
+
+  function validateLink(linkEntryType,baseHash,links,pkg,sources){
+      debug("validate link: "+linkEntryType);
+      if (linkEntryType=="app_agent_id_link") {
+          var length = links.length;
+          // a valid user_directory_link is when:
+          // there should just be one or two links only
+          if (length==2) {
+              // if this is a modify it will have two links the first of which
+              // will be the del and the second the new link.
+              if (links[0].LinkAction != HC.LinkAction.Del) return false;
+              if (links[1].LinkAction != HC.LinkAction.Add) return false;
+          } else if (length==1) {
+              // if this is a new handle, there will just be one Add link
+              if (links[0].LinkAction != HC.LinkAction.Add) return false;
+          } else {return false;}
+
+        return true;
+      }
+      return true;
+  }
+  function validateMod(entry_type,entry,header,replaces,pkg,sources) {
+      debug("validate mod: "+entry_type+" header:"+JSON.stringify(header)+" replaces:"+JSON.stringify(replaces));
+      return true;
+  }
+  function validateDel(entry_type,hash,pkg,sources) {
+      debug("validate del: "+entry_type);
+      return true;
+  }
+
+  function validatePutPkg(entry_type) {return null;}
+  function validateModPkg(entry_type) { return null;}
+  function validateDelPkg(entry_type) { return null;}
+  function validateLinkPkg(entry_type) { return null;}
