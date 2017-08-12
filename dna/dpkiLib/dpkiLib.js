94de7415202dfcf95600687f733b2d2bc618a5cf
Joel U <joelulahanna@gmail.com>
2017-08-11 17:06:59 -0700

    updates to the dev function in the DPKI

--- dna/dpkiLib/dpkiLib.js
+++ dna/dpkiLib/dpkiLib.js
@@ -1,31 +1,8 @@
 /***********
 File for library of Methods
 ************/
-/*
-function doGetTagLinkLoad(base, tag) {
-    // get the tag from the base in the DHT
-    var links = getLink(base, tag,{Load:true});
-    if (isErr(links)) {
-      debug("isErr");
-        links = [];
-    } else {
-
-       links = links.Links;
-    }
-    var links_filled = [];
-    for (var i=0;i <links.length;i++) {
-        var link = {H:links[i].H};
-        link[tag] = links[i].E;
-        links_filled.push(link);
-    }
-    debug("Links Filled:"+JSON.stringify(links_filled));
-    return links_filled;
-}
-*/
 // TODO This function will be used by the other application's
 //  by passing a public_key of the users they want to verify (i.e if he exist in the DHT of the DPKI)
-
-
 
 function bridgeGenesis()
 {
@@ -34,28 +11,63 @@ function bridgeGenesis()
 
 function receive(from, arg)
 {
-  if(arg.type == "verifyUser")
-  {
-    var val = verifyUser(arg.app_agent_id)
-  }
-  else if(arg.type == "registerUserApp")
+  //registeres the user to the dpki during genesis
+  if(arg.type == "registerUserApp")
   {
     var val=registerUserApp(arg.app_agent_id)
   }
+  //user has to register him self to the key in the DPKI
+  else if(arg.type == "registerToKey")
+  {
+    var val=registerToKey(arg.app_agent_id)
+  }
+  //Checks is the agent has registered key in the DPKI and register's the key
+  else if(arg.type == "hasRegisteredKey")
+  {
+    var val = hasRegisteredKey(arg.app_agent_id)
+  }
+  //This would give you the user details if hes registered and false is he's not registered
+  else if(arg.type == "getUserDetails")
+  {
+    var val=getUserDetails(arg.app_agent_id)
+  }
+
   return val;
 }
 
 function registerUserApp(app_agent_id){
-  keyRegistration=getLink(App.Agent.Hash,"keyRegistration",{Load:true});
-  debug(keyRegistration)
-  keyRegistrationHash=makeHash(keyRegistration.Links[0].E)
-  z=commit("app_agent_id_link", {Links:[{Base:app_agent_id,Link:keyRegistrationHash,Tag:"app_agent_id"}]});
+  z=commit("app_agent_id_link", {Links:[{Base:App.Agent.Hash,Link:app_agent_id,Tag:"app_agent_id"}]});
   debug(z)
-  debug("app_agent_id_link: "+JSON.stringify(getLink(app_agent_id,"app_agent_id",{Load:true})));
-  a=getLink(app_agent_id,"app_agent_id",{Load:true})
+  debug("app_agent_id_link: "+JSON.stringify(getLink(App.Agent.Hash,"app_agent_id",{Load:true})));
+  a=getLink(App.Agent.Hash,"app_agent_id",{Load:true})
   return true
+
 }
 
+function registerToKey(app_agent_id){
+  keyRegistration=getLink(App.Agent.Hash,"keyRegistration",{Load:true});
+  debug(keyRegistration)
+  if (isErr(keyRegistration)) {return false}
+  if (keyRegistration != undefined) {
+  keyRegistrationHash=makeHash(keyRegistration.Links[0].E)
+  debug("MAKE HASH : ----->"+keyRegistrationHash)
+  z=commit("app_agent_id_link", {Links:[{Base:app_agent_id,Link:keyRegistrationHash,Tag:"app_agent_register"}]});
+  debug(z)
+  debug("app_agent_id_link: "+JSON.stringify(getLink(app_agent_id,"app_agent_register",{Load:true})));
+  a=getLink(app_agent_id,"app_agent_register",{Load:true})
+  return true
+}
+return false
+}
+
+function hasRegisteredKey(app_agent_id){
+  key=getLink(app_agent_id,"app_agent_register",{Load:true})
+  if (isErr(key)) {return false}
+  if (key != undefined) {
+  return true
+  }
+  return false
+}
 
 function verifyUser(app_agent_id){
   var sources = get(app_agent_id,{GetMask:HC.GetMask.Sources});
@@ -70,18 +82,22 @@ function verifyUser(app_agent_id){
 
 
 function getUserDetails(app_agent_id){
-  key=getLink(app_agent_id,"app_agent_id",{Load:true})
-  source=JSON.parse(key.Links[0].E)
-  a=getLink(source.perm_dpki_id,"users",{Load:true})
-  userDetails=JSON.parse(a.Links[0].E)
-  arg={
+  key=getLink(app_agent_id,"app_agent_register",{Load:true})
+  if (isErr(key)) {return false}
+  if (key != undefined) {
+    source=JSON.parse(key.Links[0].E)
+    a=getLink(source.perm_dpki_id,"users",{Load:true})
+    userDetails=JSON.parse(a.Links[0].E)
+    arg={
     key : key.Links[0].E,
     shared_ID : userDetails.shared_ID,
     address : userDetails.address,
     email : userDetails.email
+    }
+    debug(arg);
+    return JSON.stringify(arg)
   }
-  debug(arg);
-  return JSON.stringify(arg)
+return false
 }
 /*
 //This is just going to check if the userAddress that was given actually exits
@@ -95,10 +111,11 @@ function getAgent(app_agent_id) {
     }
     return "";
 }
+*/
 function isErr(result) {
     return ((typeof result === 'object') && result.name == "HolochainError");
   }
-*/
+
 
 
   // ===============================================================================
